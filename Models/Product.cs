d698b517e8abfb5c185bb7ff7d0e88f33f24bfea
pc <pc@DESKTOP-RF36KRC>
2025-04-11 21:40:11 +0300

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace simple_application_core_api.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 10000)]
+        public decimal Price { get; set; }
+    }
+}
